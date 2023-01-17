740a4290102d0b164fcbb10d6c4949c436dfa617
Niks156 <nayangarg408@gmail.com>
2023-01-17 14:03:28 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Formcard from "./components/Formcard";
 import "./App.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 export default function App() {
   return (
--- src/components/Formcard.js
+++ src/components/Formcard.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React from "react";
+import TextField from "@mui/material/TextField";
 import "../App.css";
 
 export default function Formcard() {
   return (
-    <div className='form-card'>
-      card
+    <div className="form-card">
+      <div className="form-header">
+        <div className="header-img flex">
+          <img
+            src={require("../img/header.png")}
+            alt="header"
+            width={51}
+            height={48}
+          />
+        </div>
+        <div className="form-heading">It’s a delight to have you onboard</div>
+        <div className="form-sub-heading">
+          Help us know you better. <br /> (This is how we optimize Wobot as per
+          your business needs)
+        </div>
+      </div>
+      <div className="form-body label">
+        <label for="filled-hidden-label-small">Company Name</label>
+        <TextField
+          hiddenLabel
+          id="filled-hidden-label-small"
+          className="input"
+        />
+      </div>
+      <div className="form-footer"> </div>
     </div>
-  )
+  );
 }
