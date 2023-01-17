bcc0a20e294e58d35820d6b85431ccffa3c36d69
Niks156 <nayangarg408@gmail.com>
2023-01-17 12:43:37 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Formcard from "./components/Formcard";
+import "./app.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="logo">
+        <img
+          src={require("./img/logo.png")}
+          alt="wobot"
+          height={36.02}
+          width={160}
+        />
+      </div>
+      <div className="form-container">
+        <div className="left-container"></div>
+        <div className="form">
+          <Formcard />
+        </div>
+        <div className="right-container"></div>
+      </div>
+      <div className="footer-container">
+        <span>Terms of use | </span>
+        <span>Privacy policy</span>
+      </div>
     </div>
   );
 }
-
-export default App;
